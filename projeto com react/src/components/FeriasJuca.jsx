aae5da3813b908ed6df362d08cf83bc853bc6fec
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-02-25 22:15:14 -0300

    Fechei muito cedo

--- projeto com react/src/components/FeriasJuca.jsx
+++ projeto com react/src/components/FeriasJuca.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { userState } from 'react-dom'
 function conferir() {
-
+    cosnt[conta, setConta] = userState
 }
 function FeriasJuca() {
+    let diarias = Number(prompt("Quantos Dias?"))
+    let valorDiaria
+
+    if (diarias <= 5){
+        valorDiaria = 100
+    } else if (diarias < 11) {
+        valorDiaria = 90
+    } else {
+        valorDiaria = 80
+    }
+
+    let valorBruto = diarias * valorDiaria
+
+    let desconto10 = valorBruto * 0.10
+    let desconto15 = valorBruto * 0.15
+    let valorTotal = valorBruto - desconto10 - desconto15 + 150
     return (
         <div className={"albergue"}>
             <h2>Hotel de Cria</h2>
