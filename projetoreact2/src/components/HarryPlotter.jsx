2848830f9c23daf3601c98bb5548afb015fa5046
CaioRO777 <mamilocremoso235@gmail.com>
2026-03-03 13:41:48 -0300

    Agora a segunda atividade está completa :/

--- projetoreact2/src/components/HarryPlotter.jsx
+++ projetoreact2/src/components/HarryPlotter.jsx
@@ -1,19 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 
 function HarryPlotter() {
-
+    const [tabela, setTabela] = useState([])
+    
     function tabela_preco() {
         let preco = []
 
-        for (let i = 1; i < 500; i++) {
+        for (let i = 1; i <= 500; i++) {
             let resultado = 0.33 * i;
-        }}
-            return (
-                <div>
-                    <h2> Tabela de Preço </h2>
-                    <button onClick={tabela_preco}>Verificar Preoços</button>
-                </div>
-            )
+            preco.push({ quantidade: i, preco: resultado });
         }
+        
+        setTabela(preco);
+    }
+    
+    return (
+        <div>
+            <h2> Tabela de Preços </h2>
+            <button onClick={tabela_preco}>Verificar Preços</button>
+            
+            {/* Cuidado com a estrutura do map aqui! */}
+            <ul>
+                {tabela.map((item) => (
+                    <li key={item.quantidade}>
+                        {item.quantidade} item(ns) - R$ {item.preco.toFixed(2)}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default HarryPlotter
