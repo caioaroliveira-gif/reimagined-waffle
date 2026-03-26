1555a7f559894716499e73880ab61aeecd5aca65
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-03-25 22:17:47 -0300

    Começando a aprender sobre layout

---
+++ projeto com react 4/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="cont-app">
+        <header className={'cont-header'}>Nome/logo/links</header>
+        <main className={'cont-main'}>Aqui as coisas acontecem e aparecem</main>
+        <section>Aleatoria</section>
+        <footer>Rodape</footer>
+      </div>
+
+    </>
+  )
+}
+
+export default App
