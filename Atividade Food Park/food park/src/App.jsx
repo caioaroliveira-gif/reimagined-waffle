b8c0f27bc4f8103108f46b5b9c51fa4da301e3f6
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-04-01 22:13:35 -0300

    a

---
+++ Atividade Food Park/food park/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className={'cont-main'}>
+      <h1>Food Park Comidos</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
