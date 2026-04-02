20efea6ffd5c66390d63f1d25ca58386a07687d8
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-04-01 21:52:30 -0300

    Aprendendo Layouts.

--- projeto com react 4/src/App.jsx
+++ projeto com react 4/src/App.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Produtos from './pages/Produtos';
 
 function App() {
+  const [tela, setTela] = useState(<Home />)
 
   return (
     <>
       <div className="cont-app">
-        <header className={'cont-header'}>Nome/logo/links</header>
-        <main className={'cont-main'}>Aqui as coisas acontecem e aparecem</main>
-        <section>Aleatoria</section>
-        <footer>Rodape</footer>
+        <header className={'cont-header'}>
+          <nav>
+            <button className={'botoes-nav'} onClick={() => setTela(<Home />)}>
+              Home</button>
+            <button className={'botoes-nav'} onClick={() => setTela(<Contato />)}>
+              Contato</button>
+            <button className={'botoes-nav'} onClick={() => setTela(<Produtos />)}>
+              Produto</button>
+
+          </nav>
+        </header>
+        <main className={'cont-main'}>
+          {tela}
+        </main>
+
       </div>
+
+
 
     </>
   )
